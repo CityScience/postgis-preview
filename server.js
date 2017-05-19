8e71314a7f4cec38b03b4393cdd1c2d6dffb1136
Louise Moxhay <louise.moxhay@cityscience.com>
2017-05-19 09:38:14 +0100

    Correctly fixed the db log in issue Also errors now show in the browser not just in the terminal.

--- server.js
+++ server.js
@@ -21,15 +21,19 @@ var app = express(),
     port = process.env.PORT || 4000,
     connectionParams  = process.env.DATABASE_URL || {},
     db = pgp(connectionParams ),
-    poolConfig = {max: 5, min: 1}
+    poolConfig = {max: 5, min: 1, 
+        database: process.env.PGUSER,
+        user: process.env.PGUSER,
+        port: process.env.PGPORT}
     pool = new Pool(poolConfig);
 
 //use express static to serve up the frontend
 app.use(express.static(__dirname + '/public'));
 
-
 app.get('/sql', (req, res) => {
     var sql = req.query.q;
+    var format = req.query.format || 'topojson';
+    console.log('Executing SQL: ' + sql, format);
     res.set('Content-Type', 'text/plain');
     pool.connect(function(err, client, release){        // get a connection from the pool
         if(err) {
@@ -62,64 +66,6 @@ app.get('/sql', (req, res) => {
     });
 });
 
-// app.get('/sq1', function(req, res, next){
-//     var c = 0;
-//     pg.connect(connectionParams, (err, client, done) => {
-//         // Handle connection errors
-//         if(err) {
-//             done();
-//             console.log(err);
-//             return res.status(500).json({success: false, data: err});
-//         }
-//         // SQL Query > Select Data
-//         const query = client.query(sql);
-//         // Stream results back one row at a time
-//         query.on('row', (row) => {
-//             res.setHeader('Content-Type', 'application/json');
-//             res.send(JSON.stringify(row));
-//             c++;
-//         });
-//         // After all data is returned, close connection and return results
-//         query.on('end', () => {
-//             console.log('Chunks: ' + c);
-//             // return res.send(results);
-//             res.status(200).end();
-//         });
-//     });
-// });
-
-//     // query using pg-promise
-//     db.any(any)
-//         .then(function (data) { //use dbgeo to convert WKB from PostGIS into topojson
-//             switch (format) {
-//                 case 'csv':
-//                     return jsonExport(data).then(function (data) {
-//                         res.setHeader('Content-disposition', 'attachment; filename=query.csv');
-//                         res.setHeader('Content-Type', 'text/csv');
-//                         return data;
-//                     });
-//                 case 'geojson':
-//                     return dbGeoParse(data, format).then(function (data) {
-//                         res.setHeader('Content-disposition', 'attachment; filename=query.geojson');
-//                         res.setHeader('Content-Type', 'application/json');
-//                         return data;
-//                     });
-//                 default:
-//                     return 
-//             }
-//         })
-//         .then(function (data) {
-//             return res.send(data);
-//         })
-//         .catch(function (err) { //send the error message if the query didn't work
-//             var msg = err.message || err;
-//             console.log("ERROR:", msg);
-//             res.send({
-//                 error: msg
-//             });
-//         });
-// });
-
 function dbGeoParse(){
     return through(function write(row) {
         row['geom'] = wkx.Geometry.parse(new Buffer(row['geom'], 'hex')).toGeoJSON()
@@ -128,19 +74,6 @@ function dbGeoParse(){
         this.queue(null);
     });
 }
-// function dbGeoParse(data, format) {
-//     return new Promise(function (resolve, reject) {
-//         dbgeo.parse(data, {
-//             outputFormat: format
-//         }, function (err, result) {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(result);
-//             }
-//         });
-//     });
-// }
 
 
 function jsonExport(data) {
